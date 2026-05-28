0fb9ab2155f4c645db8a0dc705ad58e3adc36dbf
Andriy Kushnarov <andriyku@google.com>
2026-05-28 14:53:21 +0200

    Upgrade angular to v21

--- src/ui/src/main.ts
+++ src/ui/src/main.ts
@@ -1,3 +1,4 @@
+import { provideZoneChangeDetection } from '@angular/core';
 /**
  * Copyright 2024 Google LLC
  *
@@ -17,4 +18,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [provideZoneChangeDetection(), ...appConfig.providers],
+}).catch(err => console.error(err));
